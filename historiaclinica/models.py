4836226279289a594b615f8451192d89afb77dd2
Juan Alvarez <juangabo.0228@gmail.com>
2013-06-06 18:47:31 -0500

    finishes models

--- historiaclinica/models.py
+++ historiaclinica/models.py
@@ -7,7 +7,6 @@ class Paciente(models.Model):
     segundo_apellido = models.CharField(max_length=30)
     fecha_nacimiento = models.DateTimeField(max_length=30)
     lugar_nacimiento = models.CharField(max_length=30)
-    edad = models.CharField(max_length=20) 
     TIPO_DOCUMENTO_CHOICES = (
         ('RC', 'Registro Civil'),
         ('TI', 'Tarjeta de Identidad'),
@@ -22,12 +21,12 @@ class Paciente(models.Model):
     ciudad = models.CharField(max_length=30)
     departamento = models.CharField(max_length=30)
     telefono = models.CharField(max_length=10)
-    motivo_consulta = models.CharField(max_length=1000)
     SEXO_CHOICES = (
         ('M', 'Masculino'),
         ('F', 'Femenino'),
         )
     sexo = models.CharField(max_length=1, choices=SEXO_CHOICES, default='M')
+
 class HistoriaClinica(models.Model):
     fecha_toma_examen = models.DateTimeField(max_length=30)
     cuadro_clinico = models.CharField(max_length=1000)
@@ -38,3 +37,4 @@ class HistoriaClinica(models.Model):
     talla = models.FloatField(max_length=30)
     peso = models.FloatField(max_length=30)
     paciente = models.ForeignKey(Paciente, related_name='historias')
+    motivo_consulta = models.CharField(max_length=1000)
